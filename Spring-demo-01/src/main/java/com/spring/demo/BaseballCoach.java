0af919429a0779855e5bf76cb8f30fe0b8a17884
Ornelo Nazare <nazareornelo@yahoo.com>
2017-03-13 01:40:37 +0100

    First java spring example

---
+++ Spring-demo-01/src/main/java/com/spring/demo/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.spring.demo;
+
+public class BaseballCoach implements Coach {
+
+
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 30 minutes on batting practice";
+	}
+	
+	
+}
